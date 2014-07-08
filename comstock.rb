7dad848e78780a8122f7e11b77c06fa091dc676d
shinpei <shinpei.nakata@gmail.com>
2014-07-08 21:17:48 +0900

    update repo url

--- comstock.rb
+++ comstock.rb
@@ -3,7 +3,7 @@ require "formula"
 class Comstock < Formula
   homepage "http://comstock.herokuapp.com"
   version '0.1.3'
-  url "http://dl.bintray.com/shinpei/generic/generic/comstock/0.1.3/comstock"
+  url "http://dl.bintray.com/shinpei/generic/comstock/0.1.3/comstock"
   sha1 "18b6ba25e83608df86ceb51007657cb15afdc54b"
   def install
     bin.install 'comstock'
