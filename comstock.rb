6fd05e9fd8c4f58184602fdad6e62a788c1e12a7
shinpei <shinpei.nakata@gmail.com>
2014-07-08 21:13:16 +0900

    v0.1.3

--- comstock.rb
+++ comstock.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Comstock < Formula
   homepage "http://comstock.herokuapp.com"
-  version '0.1.2'
-  url "http://dl.bintray.com/shinpei/generic/generic/comstock/0.1.2/comstock"
-  sha1 "63b38d4f9a6406a61f65a8ef2d90533979d3cba3"
+  version '0.1.3'
+  url "http://dl.bintray.com/shinpei/generic/generic/comstock/0.1.3/comstock"
+  sha1 "18b6ba25e83608df86ceb51007657cb15afdc54b"
   def install
     bin.install 'comstock'
   end
