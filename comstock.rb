8ceaa7aed25053172adaa9b53624c7e915cdc55e
shinpei <shinpei.nakata@gmail.com>
2014-08-03 16:47:21 +0900

    update

--- comstock.rb
+++ comstock.rb
@@ -4,7 +4,7 @@ class Comstock < Formula
   homepage "http://comstock.herokuapp.com"
   version '0.1.4'
   url "http://dl.bintray.com/shinpei/generic/gemeric/comstock/0.1.4/comstock_0.1.4_darwin_amd64.zip"
-  sha1 "36ab57b1fe46e47d74c07a9974c27b8389d796ce"
+  sha1 "ee3fcffb3ba45c82272e83b7e2c0f30a404e89af"
 
   def install
     bin.install 'comstock'
