9c27768365ca3b18dfbeb8ccfac69ae729f003de
shinpei <shinpei.nakata@gmail.com>
2014-08-27 01:55:30 +0900

    release v0.1.4

--- comstock.rb
+++ comstock.rb
@@ -4,11 +4,11 @@ class Comstock < Formula
   homepage "http://comstock.herokuapp.com"
   version '0.1.4'
   url "http://dl.bintray.com/shinpei/generic/comstock/0.1.4/comstock_0.1.4_darwin_amd64.zip"
-  sha1 "c7dc080254acd838a37220930ed98c60b5e64488"
+  sha1 "23a17844f8f56707ebb236cadae0ba6a902aec78"
 
   def install
+    bin.install 'comstock-cli'
     bin.install 'comstock'
-    bin.install 'coms'
     bin.install 'coms_save_previous'
   end
 end
