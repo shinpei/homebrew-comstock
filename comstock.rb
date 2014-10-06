6b64582d245782fee9fc85d985f9391026361a81
shinpei <shinpei.nakata@gmail.com>
2014-10-06 22:33:39 +0900

    update skip_clean

--- comstock.rb
+++ comstock.rb
@@ -5,11 +5,13 @@ class Comstock < Formula
   version '0.1.8'
   url "https://github.com/shinpei/comstock/releases/download/0.1.8/comstock_0.1.8_darwin_amd64.zip"
   sha1 "2552672cef76c375828c2d243a4a01c31cef9954"
-  skip_clean 'comstock', 'coms_save_previous'
+  skip_clean "bin"
+
   def install
     bin.install 'comstock-cli'
     bin.install 'comstock'
+    
     bin.install 'coms_save_previous'
-    ##bin.install 'coms_save_previous'
+
   end
 end
