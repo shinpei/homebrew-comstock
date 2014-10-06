ff9f979029d63792699459a3fadb728cfaf2d4ee
shinpei <shinpei.nakata@gmail.com>
2014-10-06 21:50:25 +0900

    update

--- comstock.rb
+++ comstock.rb
@@ -8,7 +8,8 @@ class Comstock < Formula
 
   def install
     bin.install 'comstock-cli'
-    bin.write_exec_script 'comstock'
-    bin.write_exec_script 'coms_save_previous'
+    bin.install 'comstock'
+    //bin.write_exec_script 'coms_save_previous'
+    bin.install 'comstock_save_previous'
   end
 end
