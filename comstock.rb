e4f9ab7a9af6a684d92251c70a316d4d20421637
shinpei <shinpei.nakata@gmail.com>
2014-10-07 19:12:06 +0900

    release 0.1.9

--- comstock.rb
+++ comstock.rb
@@ -2,16 +2,14 @@ require "formula"
 
 class Comstock < Formula
   homepage "http://comstock.herokuapp.com"
-  version '0.1.8'
-  url "https://github.com/shinpei/comstock/releases/download/0.1.8/comstock_0.1.8_darwin_amd64.zip"
-  sha1 "2552672cef76c375828c2d243a4a01c31cef9954"
+  version '0.1.9'
+  url "https://github.com/shinpei/comstock/releases/download/0.1.9/comstock_0.1.9_darwin_amd64.zip"
+  sha1 "35bbb843a1040475b1ee74855a227192d48e9be2"
   skip_clean "bin"
 
   def install
     bin.install 'comstock-cli'
     bin.install 'comstock'
-    
     bin.install 'coms_save_previous'
-
   end
 end
