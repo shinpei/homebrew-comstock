24f77a293900e59bda0cb286c8d54bd8ab0c1148
shinpei <shinpei.nakata@gmail.com>
2014-12-25 01:08:48 +0900

    release pre2 finally

--- comstock.rb
+++ comstock.rb
@@ -4,7 +4,7 @@ class Comstock < Formula
   homepage "http://comstock.herokuapp.com"
   version '0.2.0-pre2'
   url "https://github.com/shinpei/comstock/releases/download/0.2.0-pre2/comstock_0.2.0-pre2_darwin_amd64.zip"
-  sha1 "9a2242ca35608962f53942cc9943427b75b8e689"
+  sha1 "a238c828ac0f52e14eca34a99af8ffc0d2a626a2"
   skip_clean "bin"
 
   def install
